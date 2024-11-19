031648aed4de706ac043ad13c19be1e8f7683fef
Aranfraij <aran.fraij@post.runi.ac.il>
2024-11-19 21:12:01 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,19 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	       String text = args[0];
+            int times = Integer.parseInt(args[1]);
+            for (int i = 0; i < text.length(); i++) {
+                char c = text.charAt(i);
+                            if ("AEIOUaeiou".indexOf(c) != -1){
+                                    System.out.println("Give me an " + c + ": " + c + "!");
+                            } else {
+                                    System.out.println("Give me a " + c + ": " + c + "!");
+                            }
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < times; i++) {
+                System.out.println(text + "!!!");
+            }
         }
-}
+
